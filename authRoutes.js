9a16bc08ae2c821724c7e0bea6ac928beb5f69d9
mor <morpariente1@gmail.com>
2023-09-26 01:54:35 +0300

    2ndtry

---
+++ authRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+router.get("/api/auth/google/callback", passport.authenticate("google", { failureRedirect: "/failed" }),
+  async function (req, res) {
+    if (req.user) {
+      const userEmail = req.user.emails[0].value;
+      const userName = req.user.displayName;
+      const user = {
+        name: userName,
+        email: userEmail,
+      };
+
+      const client = new MongoClient(mongoUrl, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      });
+
+      try {
+        await client.connect();
+        await client.db("social").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const db = client.db('social');
+        const result = await db.collection('users').insertOne(user);
+        console.log(`Inserted user with ID: ${result.insertedId}`);
+      } catch (err) {
+        console.error("Error inserting user into MongoDB:", err);
+      } finally {
+        await client.close();
+      }
+    }
+    res.redirect("/good");
+  });
+
+module.exports = router;
---
+++ passport-setup.js
@@ -0,0 +1,22 @@
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+passport.use(new GoogleStrategy({
+  clientID: "***REMOVED***",
+  clientSecret: "***REMOVED***",
+  callbackURL: "http://localhost:3000/api/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  // use the profile info (mainly profile id) to check if the user is registered in your database
+    return done(null, profile);
+  }));
